d7fe0f37e537362e1bff34a8968358d5359db655
kroko <code@kroko.me>
2018-01-23 05:20:51 +0200

    syntax fix

--- 03-webpack-guide-htmlbuild/webpacktest-htmlbuild/.postcssrc.js
+++ 03-webpack-guide-htmlbuild/webpacktest-htmlbuild/.postcssrc.js
@@ -8,14 +8,14 @@ module.exports = (ctx) => ({
     require('css-mqpacker')(),
     // we always minimise CSS, also on dev, as we have source maps, but this shows how we can make this env aware
     ctx.env === 'development'
-    ? null
-    : require('cssnano')({
-      discardComments: {
-        removeAll: true
-      },
-      autoprefixer: false,
-      zindex: false,
-      normalizeUrl: false
-    })
+      ? null
+      : require('cssnano')({
+        discardComments: {
+          removeAll: true
+        },
+        autoprefixer: false,
+        zindex: false,
+        normalizeUrl: false
+      })
   ].filter((e) => e !== null)
 });
