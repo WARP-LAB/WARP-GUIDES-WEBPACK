53acf89003f8c40c075c6d220502b53c70f924aa
kroko <code@kroko.me>
2018-01-25 02:47:43 +0200

    updates in preflight and html to give more context for css state machine

--- 04-webpack-guide-babel/webpacktest-babel/src/preflight.js
+++ 04-webpack-guide-babel/webpacktest-babel/src/preflight.js
@@ -1,3 +1,25 @@
-// change noscript to script in html tag
+'use strict';
+
+// PREFILIGHT HAS TO STAY ES3 COMPATIBLE
+// Inline in head.
+// This will pause HTML parsing and execute immediately.
+// It changes preceding DOM.
+// When we hit body CSS state machine will be already setup.
+
+// ############################################################
+// Change noscript to script.
 document.documentElement.className = document.documentElement.className.replace(/\bnoscript\b/, 'script');
+
+// ############################################################
+// Change incapable to capable.
+// Normally we try to deploy stuff that works on ES5-ish browsers.
+// That means even down to IE9 although that denies usage of many nice tools.
+// If somebody asked to deliver IE8 capable solution, then we would have to use different tools.
+// As example this CTM will inform us via CSS state machine that we are IE10+.
+if ('visibilityState' in document) {
+  document.documentElement.className = document.documentElement.className.replace(/\bincapable\b/, 'capable');
+}
+
+// ############################################################
+// Just say hello.
 console.log('I am Preflight');
